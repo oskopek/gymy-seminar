a1c97063168ce18f4445b9b3be1e27210e9a34e3
Ondrej Skopek <skopekondrej@gmail.com>
2014-09-11 17:06:21 +0200

    Adds SeminarPanelTest

---
+++ src/test/java/sk/gymy/seminar/swingui/SeminarPanelTest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2014 Ondrej Skopek
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package sk.gymy.seminar.swingui;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.optaplanner.core.api.solver.Solver;
+import org.optaplanner.core.api.solver.SolverFactory;
+import sk.gymy.seminar.app.SeminarApp;
+import sk.gymy.seminar.domain.Groups;
+import sk.gymy.seminar.persistence.SeminarImporter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
+public class SeminarPanelTest {
+
+    private SeminarPanel panel;
+
+    private static Groups groups;
+    private static Groups solvedGroups;
+
+    @BeforeClass
+    public static void setUpClass() {
+        groups = (Groups) new SeminarImporter().readSolution(new File("data/seminar/import/simple5.sem"));
+        assertNotNull(groups);
+        assertEquals("Seminar-simple-5", groups.getName());
+        assertEquals(3, groups.getN());
+        assertEquals(20, groups.getStudentList().size());
+        assertEquals(3, groups.getTeacherList().size());
+        assertEquals(5, groups.getSeminarList().size());
+        assertNull(groups.getScore());
+
+        solvedGroups = groups;
+        SolverFactory solverFactory = SolverFactory.createFromXmlResource(SeminarApp.SOLVER_CONFIG);
+        Solver solver = solverFactory.buildSolver();
+        solver.solve(solvedGroups);
+        solvedGroups = (Groups) solver.getBestSolution();
+        assertNotEquals(solvedGroups, groups); // Asserts, that the groups stays uninitialized
+    }
+
+    @Before
+    public void setUp() {
+        panel = new SeminarPanel();
+        assertNotNull(panel);
+        assertEquals(0, panel.getComponents().length);
+    }
+
+    @Test
+    public void testPanelUninitialized() {
+        panel.resetPanel(groups);
+        assertEquals(3, panel.getComponents().length); // 3 group labels + 0 seminar buttons
+    }
+
+    @Test
+    public void testPanelSolved() {
+        panel.resetPanel(solvedGroups);
+        assertEquals(8, panel.getComponents().length); // 3 group labels + 5 seminar buttons
+    }
+
+    @Test
+    public void testLockIconDrawing() {
+        solvedGroups.getSeminarList().get(0).setLocked(true);
+        solvedGroups.getSeminarList().get(2).setLocked(true);
+        panel.resetPanel(solvedGroups);
+        assertEquals(8, panel.getComponents().length); // 3 group labels + 5 seminar buttons
+        int buttonsWithLockIcons = 0;
+        for (Component component : panel.getComponents()) {
+            assertNotNull(component);
+            JButton button;
+            try {
+                button = (JButton) component;
+            } catch (ClassCastException cce) {
+                continue; // if component isn't a button
+            }
+            if (button.getIcon() != null) {
+                buttonsWithLockIcons++;
+            }
+        }
+        assertEquals(2, buttonsWithLockIcons); // Two locked seminars
+    }
+
+}
